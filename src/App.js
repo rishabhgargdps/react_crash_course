253834941aa72eb80af365589e9b0f20ce0747a0
rishabhgargdps <rishabhgargbits@gmail.com>
2020-08-15 12:22:00 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from "react";
+import Todos from "./components/Todos";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "H1",
+        completed: false,
+      },
+      {
+        id: 2,
+        title: "H2",
+        completed: false,
+      },
+      {
+        id: 3,
+        title: "H3",
+        completed: false,
+      },
+    ],
+  };
+  render() {
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Todos.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TodoItem from "./Todoitem";
+
+class Todos extends React.Component {
+  render() {
+    return this.props.todos.map((todo) => (
+      <TodoItem key={todo.id} todo={todo} />
+    ));
+  }
+}
+
+export default Todos;
